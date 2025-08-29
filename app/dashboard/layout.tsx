350b3dbb6ac6a2782d46a9a5d8f6d39ec389b3ba
김태헌 <7w7w7w7w@naver.com>
2025-08-29 15:41:35 +0900

    feat: 사이드바 UI 완성, 수신함 & AI 버튼 추가

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import "@/app/ui/global.css";
+import SideBar from "../ui/dashboard/side-bar";
+import SideNavBar from "../ui/dashboard/side-nav-bar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screen flex-row overflow-hidden">
+      <div className="flex h-full">
+        <div className="w-28 flex-none">
+          <SideBar />
+        </div>
+        <div className="w-64 flex-none border-l border-brown-200">
+          <SideNavBar />
+        </div>
+      </div>
+      <div className="flex-grow p-6 md:p-12 bg-brown-300">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;

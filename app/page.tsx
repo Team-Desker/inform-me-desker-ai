4efaf0856942679df8cea1eb589f34b3b5c6d7f1
김태헌 <7w7w7w7w@naver.com>
2025-08-30 19:42:57 +0900

    style:  html 구조에 맞게 태그 변경

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <main>
-      <div className="px-6 -mt-4 md:-mt-6 lg:-mt-8">
+    <>
+      <header className="px-6 -mt-4 md:-mt-6 lg:-mt-8">
         <Image
           src="/desker-logo.png"
           width={150}
@@ -12,11 +12,13 @@ const LandingPage = () => {
           alt="Desker Logo"
           priority
         />
+      </header>
+      <main>
         <div className="flex justify-center items-center -mt-2 md:-mt-3 lg:-mt-4">
-          <span className="block font-bold text-4xl text-center">
+          <h1 className="block font-bold text-4xl text-center">
             고객과 대화하는 데스커 AI 로<br />
             떠나는 고객을 붙잡아 매출로 전환하세요
-          </span>
+          </h1>
         </div>
         <div className="pb-10"></div>
         <div className="flex justify-center items-center gap-10 pb-10">
@@ -33,7 +35,7 @@ const LandingPage = () => {
             <span>무료로 체험하기</span>
           </Link>
         </div>
-        <div className="flex justify-center items-center rounded-lg bg-brown-400 p-0.5 ">
+        <section className="flex justify-center items-center rounded-lg bg-brown-400 p-0.5 ">
           <Image
             src="/information-section-image.png"
             width={1280}
@@ -41,9 +43,9 @@ const LandingPage = () => {
             alt="Landing Information Section Image"
             priority
           />
-        </div>
-      </div>
-    </main>
+        </section>
+      </main>
+    </>
   );
 };
 

589858a23d56ea391f9c41d3adaa956e27c79fe0
김태헌 <7w7w7w7w@naver.com>
2025-08-30 21:41:06 +0900

    file: 공식문서 기반 [useId] 파일명 수정

--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -2,17 +2,17 @@ import "@/app/ui/global.css";
 import SideBar from "@/app/ui/dashboard/side-bar";
 import SideNavBar from "@/app/ui/dashboard/side-nav-bar";
 
-const user_id = 1234; // 임시 user_id (확인용)
+const userId = 1234; // 임시 userId (확인용)
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen flex-row overflow-hidden">
       <div className="flex h-full">
         <div className="w-28 flex-none">
-          <SideBar user_id={user_id} />
+          <SideBar userId={userId} />
         </div>
         <div className="w-64 flex-none border-l border-brown-200">
-          <SideNavBar user_id={user_id} />
+          <SideNavBar userId={userId} />
         </div>
       </div>
       <div className="flex-grow p-6 md:p-12">{children}</div>

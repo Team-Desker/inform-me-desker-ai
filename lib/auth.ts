607e7cc92f7969adc899f1d3455e999759c274e4
김태헌 <7w7w7w7w@naver.com>
2025-09-02 05:45:13 +0900

    feat: 회원가입 시 자동 로그인 기능 및 비밀번호 검증 추가

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,48 @@
 import NextAuth from "next-auth";
 import prisma from "./prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  session: { strategy: "jwt" },
+  providers: [
+    Credentials({
+      async authorize(credentials: { email?: string; password?: string }) {
+        if (!credentials) {
+          return null;
+        }
+
+        const email = credentials.email;
+        const password = credentials.password;
+
+        if (!email || !password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: email },
+        });
+
+        if (!user || !user.passwordHash) {
+          return null;
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          user.passwordHash
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+        };
+      },
+    }),
+  ],
 });

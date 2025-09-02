5498c43e7e1fad2a94a14752a192ff6bef8f09b2
Sanghyuk Seo <sseo951124@gmail.com>
2025-09-02 15:36:34 +0900

    feat: 격리된 공간을 위해 shadow dom 형성 기능 추가

--- chatbot-widget/index.tsx
+++ chatbot-widget/index.tsx
@@ -6,7 +6,14 @@ const config = (window as any).DESKER_CHATBOT_CONFIG;
 const container = document.getElementById("desker-chatbot-widget-container");
 
 if (config && container) {
-  const root = ReactDOM.createRoot(container);
+  const shadowRoot = container.attachShadow({ mode: "open" });
+
+  const styleLink = document.createElement("link");
+  styleLink.rel = "stylesheet";
+  styleLink.href = `${config.apiBaseURL}/chatbot-widget/widget.css`;
+  shadowRoot.appendChild(styleLink);
+
+  const root = ReactDOM.createRoot(shadowRoot);
   root.render(
     <React.StrictMode>
       <App config={config} />

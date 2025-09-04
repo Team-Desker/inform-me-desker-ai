637ec10a341cb528532a31fccdcf3716312bc6ea
Sanghyuk Seo <sseo951124@gmail.com>
2025-09-05 00:58:30 +0900

    feat: localStorage TTL 7주일 만료 설정 기능 추가

--- chatbot-widget/lib/visitor.ts
+++ chatbot-widget/lib/visitor.ts
@@ -1,17 +1,21 @@
 import { createIdGenerator } from "ai";
+import { TTL_INFO } from "@/config/constants";
+import ls from "localstorage-slim";
 
 const VISITOR_ID_KEY = "desker-visitor-ai";
 const generateVisitorId = createIdGenerator({ prefix: "visitor" });
 
 export const getOrSetVisitorId = () => {
-  const existingId = localStorage.getItem(VISITOR_ID_KEY);
+  const existingId = ls.get(VISITOR_ID_KEY);
 
   if (existingId) {
     return existingId;
   }
 
   const newVisitorId = generateVisitorId();
-  localStorage.setItem(VISITOR_ID_KEY, newVisitorId);
+  ls.set(VISITOR_ID_KEY, newVisitorId, {
+    ttl: TTL_INFO.SESSION_STORAGE_VISITOR,
+  });
 
   return newVisitorId;
 };
--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,7 @@
 export const URL_INFO = {
   SERVER_BASE_URL: "http://localhost:3000",
 };
+
+export const TTL_INFO = {
+  SESSION_STORAGE_VISITOR: 7 * 24 * 60 * 60,
+};

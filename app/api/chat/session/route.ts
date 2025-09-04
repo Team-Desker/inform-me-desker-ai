e2842a2c5825f8b3eeb63a922f869492d7e6e0fc
Sanghyuk Seo <sseo951124@gmail.com>
2025-09-05 03:16:33 +0900

    fix: 채팅방 조회 API를 POST에서 분리

--- app/api/chat/session/route.ts
+++ app/api/chat/session/route.ts
@@ -2,9 +2,11 @@ import { ERROR_MESSAGE } from "@/config/constants";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export const POST = async (req: Request) => {
+export const GET = async (req: Request) => {
   try {
-    const { botId, visitorId } = await req.json();
+    const { searchParams } = new URL(req.url);
+    const botId = searchParams.get("botId");
+    const visitorId = searchParams.get("visitorId");
 
     if (!botId || !visitorId) {
       return NextResponse.json(
@@ -32,9 +34,47 @@ export const POST = async (req: Request) => {
       return NextResponse.json(
         {
           chatSessionId: latestSession.id,
-          isNewChat: false,
         },
         { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        error: {
+          code: "SESSION_NOT_FOUND",
+          message: ERROR_MESSAGE.SESSION_NOT_FOUND,
+        },
+      },
+      { status: 404 }
+    );
+  } catch (error) {
+    console.error("chat session 조회 에러: ", error);
+    return NextResponse.json(
+      {
+        error: {
+          code: "INTERNAL_SERVER_ERROR",
+          message: ERROR_MESSAGE.INTERNAL_SERVER_ERROR_SESSION,
+        },
+      },
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (req: Request) => {
+  try {
+    const { botId, visitorId } = await req.json();
+
+    if (!botId || !visitorId) {
+      return NextResponse.json(
+        {
+          error: {
+            code: "NOT_FOUND_BOTID_VISITORID",
+            message: ERROR_MESSAGE.NOT_FOUND_BOTID_VISITORID,
+          },
+        },
+        { status: 400 }
       );
     }
 
@@ -48,7 +88,6 @@ export const POST = async (req: Request) => {
     return NextResponse.json(
       {
         chatSessionId: newChatSession.id,
-        isNewChat: true,
       },
       { status: 201 }
     );

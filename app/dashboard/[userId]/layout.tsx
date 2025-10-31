bd53504316cef46bea6067b366145bbc9bf7a1e2
김태헌 <7w7w7w7w@naver.com>
2025-10-31 12:30:23 +0900

    refactoer: 로직 내부 sideBarShell 컴포넌트로 분리

--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -1,96 +1,21 @@
-import "@/app/ui/global.css";
-import Link from "next/link";
-import {
-  SidebarProvider,
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar";
-
-import {
-  Tooltip,
-  TooltipTrigger,
-  TooltipContent,
-} from "@/components/ui/tooltip";
-
-import { MessageCircleMore, ContactRound, Bot, UserCog } from "lucide-react";
-import { SIDE_BAR_TOOLTIP_MESSAGE } from "@/config/constants";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import SidebarShell from "@/app/ui/dashboard/side-bar-shell";
+import { cookies } from "next/headers";
 
 type LayoutProps = {
-  params: { userId: string };
+  params: Promise<{ userId: string }>;
   children: React.ReactNode;
 };
 
-const Layout = ({ params, children }: LayoutProps) => {
-  const { userId } = params;
-  const items = [
-    {
-      title: "MessageCircleMore",
-      url: `/dashboard/${userId}/invoices/sessions`,
-      icon: MessageCircleMore,
-      text: SIDE_BAR_TOOLTIP_MESSAGE.SESSIONS_LIST,
-    },
-    {
-      title: "ContactRound",
-      url: `/dashboard/${userId}/invoices/inquiries`,
-      icon: ContactRound,
-      text: SIDE_BAR_TOOLTIP_MESSAGE.VISITOR_CONTACT,
-    },
-    {
-      title: "Bot",
-      url: `/dashboard/${userId}/chatbot/setting`,
-      icon: Bot,
-      text: SIDE_BAR_TOOLTIP_MESSAGE.AI_SETTING,
-    },
-    {
-      title: "UserCog",
-      url: `/dashboard/${userId}/user/setting`,
-      icon: UserCog,
-      text: SIDE_BAR_TOOLTIP_MESSAGE.PERSONAL_SETTINGS,
-    },
-  ];
+const Layout = async ({ params, children }: LayoutProps) => {
+  const { userId } = await params;
 
   return (
     <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "4.5rem",
-        } as React.CSSProperties
-      }
+      defaultOpen={true}
+      style={{ "--sidebar-width": "4.5rem" } as React.CSSProperties}
     >
-      <div className="flex h-screen">
-        <Sidebar className="border-r border-foreground">
-          <SidebarContent>
-            <SidebarGroup>
-              <SidebarGroupContent>
-                <SidebarMenu className="flex flex-col gap-8 pt-9">
-                  {items.map((item) => (
-                    <SidebarMenuItem
-                      key={item.title}
-                      className="flex justify-center"
-                    >
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <Link href={item.url}>
-                            <item.icon className="h-8 w-8 text-sidebar-foreground" />
-                          </Link>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={10}>
-                          <p className="text-sm">{item.text}</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarGroup>
-          </SidebarContent>
-        </Sidebar>
-        <main className="flex-1 overflow-y-auto">{children}</main>
-      </div>
+      <SidebarShell userId={userId}>{children}</SidebarShell>
     </SidebarProvider>
   );
 };
